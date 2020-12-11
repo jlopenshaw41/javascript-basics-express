b41058c50eafe424784f003bd57b6d8e52c72192
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2020-12-11 11:58:19 +0000

    completed third challenge

--- src/app.js
+++ src/app.js
@@ -1,12 +1,33 @@
 const express = require('express');
 
-const { sayHello } = require('./lib/strings');
+const {
+  sayHello,
+  uppercase,
+  lowercase,
+  firstCharacter,
+  firstCharacters,
+} = require('./lib/strings');
 
 const app = express();
 
 app.get('/strings/hello/:string', (req, res) => {
   const string = sayHello(req.params.string);
   res.status(200).send({ result: string });
+});
+
+app.get('/strings/upper/:string', (req, res) => {
+  res.status(200).send({ result: uppercase(req.params.string) });
+});
+
+app.get('/strings/lower/:string', (req, res) => {
+  res.status(200).send({ result: lowercase(req.params.string) });
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  if (!req.query.length) {
+    res.status(200).send({ result: firstCharacter(req.params.string) });
+  }
+  res.status(200).send({ result: firstCharacters(req.params.string, req.query.length) });
 });
 
 module.exports = app;
