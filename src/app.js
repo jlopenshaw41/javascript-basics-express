8518ffc5124d3b2743fd12ae1ef66825bc9d9e11
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2020-12-11 14:25:39 +0000

    completed multiply challenges/tests

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable radix */
 const express = require('express');
 
 const {
@@ -8,7 +9,23 @@ const {
   firstCharacters,
 } = require('./lib/strings');
 
+const {
+  add,
+  subtract,
+  multiply,
+  divide,
+  power,
+  round,
+  roundUp,
+  roundDown,
+  absolute,
+  quotient,
+  remainder,
+} = require('./lib/numbers');
+
 const app = express();
+
+app.use(express.json());
 
 app.get('/strings/hello/:string', (req, res) => {
   const string = sayHello(req.params.string);
@@ -28,6 +45,40 @@ app.get('/strings/first-characters/:string', (req, res) => {
     res.status(200).send({ result: firstCharacter(req.params.string) });
   }
   res.status(200).send({ result: firstCharacters(req.params.string, req.query.length) });
+});
+
+app.get('/numbers/add/:firstNumber/and/:secondNumber', (req, res) => {
+  const firstInteger = parseInt(req.params.firstNumber);
+  const secondInteger = parseInt(req.params.secondNumber);
+
+  if (Number.isNaN(firstInteger) || Number.isNaN(secondInteger)) {
+    res.status(400).send({ error: 'Parameters must be valid numbers.' });
+  }
+  res.status(200).send({ result: add(firstInteger, secondInteger) });
+});
+
+app.get('/numbers/subtract/:firstNumber/from/:secondNumber', (req, res) => {
+  const firstInteger = parseInt(req.params.firstNumber);
+  const secondInteger = parseInt(req.params.secondNumber);
+
+  if (Number.isNaN(firstInteger) || Number.isNaN(secondInteger)) {
+    res.status(400).send({ error: 'Parameters must be valid numbers.' });
+  }
+  res.status(200).send({
+    result: subtract(secondInteger, firstInteger),
+  });
+});
+
+app.post('/numbers/multiply', (req, res) => {
+  const a = parseInt(req.body.a);
+  const b = parseInt(req.body.b);
+  if (!req.body.a || !req.body.b) {
+    res.status(400).send({ error: 'Parameters "a" and "b" are required.' });
+  }
+  if (Number.isNaN(a) || Number.isNaN(b)) {
+    res.status(400).send({ error: 'Parameters "a" and "b" must be valid numbers.' });
+  }
+  res.status(200).send({ result: multiply(a, b) });
 });
 
 module.exports = app;
